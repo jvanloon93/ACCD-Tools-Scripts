c8b5746edd1d8dc69521f763e5656da4412cdb30
Jvanloon93 <jvanloon93@gmail.com>
2019-05-20 15:47:16 -0400

    sapply not lapply

--- CEW Script.R
+++ CEW Script.R
@@ -6,6 +6,6 @@ library(usmap)
 
 ten_county <- as.vector(fips('PA', county =  c('Allegheny', 'Armstrong', 'Beaver', 'Butler', 'Fayette','Greene', 'Indiana', 'Lawrence', 'Washington', 'Westmoreland')))
 
-df_2017 <- as.data.frame(lapply(seq_along(ten_county), function(i) qcew_api(year = 2017, qtr = 'a', slice = 'area', ten_county[i])))
+df_2017 <- as.data.frame(sapply(seq_along(ten_county), function(i) qcew_api(year = 2017, qtr = 'a', slice = 'area', ten_county[i])))
 
-colnames(df_2017) 
+colnames(df_2017) <- c('Allegheny', 'Armstrong', 'Beaver', 'Butler', 'Fayette','Greene', 'Indiana', 'Lawrence', 'Washington', 'Westmoreland')
