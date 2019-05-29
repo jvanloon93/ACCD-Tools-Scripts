441ac17d90007ba5e2b71d689e34977bc9d16f11
Jvanloon93 <jvanloon93@gmail.com>
2019-05-29 14:34:44 -0400

    Using BlscrapeR

--- NAICS_LookUp_WritR.R
+++ NAICS_LookUp_WritR.R
@@ -1,22 +1,10 @@
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("concordance")
-install.packages('sqldf')
 library(rjson)
+library(blscrapeR)
 library(sqldf)
 library(dplyr)
 library(ggplot2)
-library(concordance)
 
-naics_code <- codedesc$NAICS
+naics_code <- blscrapeR::niacs
 
-naics_code <- as.numeric(as.character(naics_code))
-
-naics_description <- codedesc$NAICS.Desc
-
-naics_description <- as.character(naics_description)
-
-naics <- as.data.frame(cbind(naics_code, naics_description))
-
-write.csv(naics, file = "Naics_Lookup.csv")
+write.csv(naics_code, file = "Naics_Lookup.csv")
 
