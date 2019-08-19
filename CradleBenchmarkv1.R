e54182b52e7b6201356b0c499f4a88fc88f3541d
Jvanloon93 <jvanloon93@gmail.com>
2019-08-19 16:41:14 -0400

    Choppin'

--- CradleBenchmarkv1.R
+++ CradleBenchmarkv1.R
@@ -5,14 +5,11 @@ library(usmap)
 
 v17 <- load_variables(2017, "acs5")
 
-v17ACS1 <- load_variables(2017, "acs1")
-
-write.csv(v17, file = "v17.csv")
 
 MSA_Pop <- get_acs(
   geography = "metropolitan statistical area/micropolitan statistical area", 
   variables = "B01001_001", 
-  year = 2017, 
+  year = 2018, 
   geometry = FALSE)
 
 
@@ -33,6 +30,7 @@ St.Louis_Ed_Total<- All_Ed_Total %>%
 Pittsburgh_Ed_Total<- All_Ed_Total %>%
   subset(GEOID == 38300)
 
+
 HS_Education_vars <- c( "B15001_006", "B15001_007", "B15001_008", "B15001_009", "B15001_010", "B15001_014", "B15001_015", "B15001_016", "B15001_017", 
                         "B15001_018", "B15001_022", "B15001_023", "B15001_024", "B15001_025", "B15001_026", "B15001_030", "B15001_031", "B15001_032",
                         "B15001_033", "B15001_034", "B15001_038", "B15001_039", "B15001_040", "B15001_041", "B15001_042", "B15001_047", "B15001_048",
@@ -44,16 +42,18 @@ MSA_HS <- bind_rows(lapply(seq_along(HS_Education_vars),
                            function(i) get_acs(
                              geography = "metropolitan statistical area/micropolitan statistical area", 
                              variables = HS_Education_vars[i], 
-                             year = 2016, 
-                             geometry = FALSE)
-                           )
-                    ) 
+                             year = 2017, 
+                             geometry = FALSE)))
 
 St.Louis_HS_ED  <- MSA_HS %>%
   subset(GEOID == 41180) 
 
 Pittsburgh_HS_ED <- MSA_HS %>%
   subset(GEOID == 38300)
+
+Pittsburgh_HS_Percent <- sum(Pittsburgh_HS_ED$estimate) / sum(Pittsburgh_Ed_Total$estimate)
+
+St.Louis_HS_Percent <- sum(St.Louis_HS_ED$estimate) / sum(St.Louis_Ed_Total$estimate)
 
 Assoc_and_Higher_Education_vars <- c( "B15001_008", "B15001_009", "B15001_010", "B15001_016", "B15001_017", "B15001_018", "B15001_024", "B15001_025", "B15001_026",
                         "B15001_032", "B15001_033", "B15001_034", "B15001_040", "B15001_041", "B15001_042", "B15001_049", "B15001_050", "B15001_051",
@@ -65,25 +65,94 @@ MSA_HigherEd <- bind_rows(lapply(seq_along(Assoc_and_Higher_Education_vars),
                              geography = "metropolitan statistical area/micropolitan statistical area", 
                              variables = Assoc_and_Higher_Education_vars[i], 
                              year = 2017, 
-                             geometry = FALSE)
-)
-) 
+                             geometry = FALSE)))
+ 
 
 St.Louis_HigherED  <- MSA_HigherEd %>%
   subset(GEOID == 41180) 
 
 Pittsburgh_HigherED <- MSA_HigherEd %>%
   subset(GEOID == 38300)
-            
-Commute_time_Var <- c("B08013_001")            
 
-All_Commute_Time <- get_acs(geography = "metropolitan statistical area/micropolitan statistical area", 
-                            variables = Commute_time_Var, 
-                            year = 2017, 
-                            geometry = FALSE)
+Pittsburgh_Higher_Ed_Percent <- sum(Pittsburgh_HigherED$estimate) / sum(Pittsburgh_Ed_Total$estimate)
 
-St.Louis_Commute <- All_Commute_Time%>%
+St.Louis_Higher_Ed_Percent <- sum(St.Louis_HigherED$estimate) / sum(St.Louis_Ed_Total$estimate)
+
+
+Total_18_44_vars <- c("B15001_003", "B15001_011", "B15001_019", "B15001_044", "B15001_052", "B15001_060")
+
+HS_Education_18_to_44_vars <- c("B15001_006", "B15001_007", "B15001_008", "B15001_009", "B15001_010", "B15001_014", "B15001_015", "B15001_016", "B15001_017", 
+                                "B15001_018", "B15001_022", "B15001_023", "B15001_024", "B15001_025", "B15001_026", "B15001_047", "B15001_048",
+                                "B15001_049", "B15001_050", "B15001_051", "B15001_055", "B15001_056", "B15001_057", "B15001_058", "B15001_059", "B15001_063",
+                                "B15001_064", "B15001_065", "B15001_066", "B15001_067")
+
+Higher_Ed_18_to_44_vars <- c("B15001_008", "B15001_009", "B15001_010", "B15001_016", "B15001_017", "B15001_018", "B15001_024", "B15001_025", "B15001_026",
+                                "B15001_049", "B15001_050", "B15001_057", "B15001_058", "B15001_059", "B15001_063",
+                                "B15001_066", "B15001_067")
+
+Total_18_44 <- bind_rows(lapply(seq_along(Total_18_44_vars), 
+                                                function(i) get_acs(
+                                                  geography = "metropolitan statistical area/micropolitan statistical area", 
+                                                  variables = Total_18_44_vars[i], 
+                                                  year = 2016, 
+                                                  geometry = FALSE)))
+St.Louis_18_to_44  <- Total_18_44 %>%
   subset(GEOID == 41180) 
 
-Pittsburgh_Commute <- All_Commute_Time %>%
+Pittsburgh_18_to_44<- Total_18_44%>%
   subset(GEOID == 38300)
+
+HS_Education_18_to_44 <- bind_rows(lapply(seq_along(HS_Education_18_to_44_vars), 
+                                                function(i) get_acs(
+                                                  geography = "metropolitan statistical area/micropolitan statistical area", 
+                                                  variables = HS_Education_18_to_44_vars[i], 
+                                                  year = 2017, 
+                                                  geometry = FALSE)))
+
+St.Louis_HS_ED_18_44  <- HS_Education_18_to_44 %>%
+  subset(GEOID == 41180) 
+
+Pittsburgh_HS_ED_18_to_44 <- HS_Education_18_to_44%>%
+  subset(GEOID == 38300)
+
+Pittsburgh_HS_Ed_18_to_44_Percent <- sum(Pittsburgh_HS_ED_18_to_44$estimate)/sum(Pittsburgh_18_to_44$estimate)
+
+St.Louis_HS_ED_18_to_44_Percent <- sum(St.Louis_18_to_44$estimate)/sum(St.Louis_18_to_44$estimate)
+
+Higher_Ed_18_to_44 <- bind_rows(lapply(seq_along(Higher_Ed_18_to_44_vars), 
+                                                          function(i) get_acs(
+                                                            geography = "metropolitan statistical area/micropolitan statistical area", 
+                                                            variables = Higher_Ed_18_to_44_vars[i], 
+                                                            year = 2017, 
+                                                            geometry = FALSE)))
+
+St.Louis_HigherED_18_to_44  <- Higher_Ed_18_to_44 %>%
+  subset(GEOID == 41180) 
+
+Pittsburgh_HigherED_18_to_44 <- Higher_Ed_18_to_44%>%
+  subset(GEOID == 38300)
+
+Pittsburgh_HigherED_18_to_44_Percent <- sum(Pittsburgh_HigherED_18_to_44$estimate) / sum(Pittsburgh_18_to_44$estimate)
+
+St.Louis_HigherED_18_to_44_Percent <- sum(St.Louis_HigherED_18_to_44$estimate) / sum(St.Louis_18_to_44$estimate)
+
+
+Pittsburgh_Pop$estimate
+
+Pittsburgh_HS_Percent
+
+Pittsburgh_Higher_Ed_Percent
+
+Pittsburgh_HS_Ed_18_to_44_Percent
+
+Pittsburgh_HigherED_18_to_44_Percent
+
+
+MSA_Fips <- c("4238300", "2941180")
+
+Unemployment_MSA_codes <- as.vector(sapply(seq_along(MSA_Fips), function(i) (paste('LAUMT', MSA_Fips[i],'0000000004', sep = ""))))
+
+Unemployment <-bls_api(c(Unemployment_MSA_codes[1]))
+
+
+
