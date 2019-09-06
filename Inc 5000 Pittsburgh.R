250ab5f27f8977578da61a3ef71cb0679951b530
Jvanloon93 <jvanloon93@gmail.com>
2019-09-06 12:59:16 -0400

    Cleaned Dpylr grammar

--- Inc 5000 Pittsburgh.R
+++ Inc 5000 Pittsburgh.R
@@ -5,19 +5,14 @@ library(ggplot2)
 df <- read.csv("https://query.data.world/s/lg5qczh7bbx2wfu6ckr6qkap57wq53", header=TRUE, stringsAsFactors=FALSE);
 
 counts <- df %>%
-  count(df$X_...metro) 
-
+  count(df$X_...metro) %>%
+  rename(Geography = 'df$X_...metro') %>%
+  mutate(Pittsburgh = ifelse(Geography == 'Pittsburgh, PA', T, F)) %>%
+  arrange(desc(n))
+  
 counts  <- counts[-1, ] 
 
-counts <- arrange(counts, desc(n))
-
-counts <- counts %>% 
-  rename(Geography = 'df$X_...metro')  
-
 counts$Geography <- factor(counts$Geography, levels = counts$Geography[order(counts$n, decreasing = TRUE)])
-
-counts <- counts %>%
-  mutate(Pittsburgh = ifelse(Geography == 'Pittsburgh, PA', T, F))
 
 ggplot(counts, aes(x = Geography, y = n, fill = Pittsburgh)) + 
   geom_col()+ 
